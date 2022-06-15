f432879d19773f6874c380fcbfa6f8b8a311d0ea
Yash Garala <yash.garala@bacancy.com>
2022-06-15 18:21:19 +0530

    file url get

--- app/controllers/user_details_controller.rb
+++ app/controllers/user_details_controller.rb
@@ -3,13 +3,17 @@ class UserDetailsController < ApplicationController
     before_action :set_user_detail
     def show
         temp =UserDetail.find_by(users_id: @curent_user.id)
-        render json: temp , status: 200
+        user_image_url= Rails.application.routes.url_helpers.rails_blob_path(temp.avatar, only_path: true)
+        p user_image_url
+        render :json => {user_details: temp,:user_image_url=> user_image_url} , status: 200
     end
     def update
         user_detail=UserDetail.find_by(users_id:@curent_user.id)
         user_detail.update(user_detail_params)
+
+        render json: user_detail , status: 200
     end
-    
+
     private
     def set_user_detail
         @curent_user=Token.new.get_user_from_token(request)
