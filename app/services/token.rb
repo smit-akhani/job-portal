d613fba5d0cc8fa44c11fbb314ffe865cb08b410
Smit Akhani <smit.akhani@bacancy.com>
2022-06-15 18:19:48 +0530

    devise company

--- app/services/token.rb
+++ app/services/token.rb
@@ -4,4 +4,11 @@ class Token
         user_id = jwt_payload['sub']
         User.find(user_id.to_s)
     end
+
+    def get_company_from_token(request)
+        jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1], Rails.application.credentials.devise[:jwt_secret_key]).first
+        company_id = jwt_payload['sub']
+        p company_id
+        Company.find(company_id.to_s)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :companies,
+              controllers: {
+                sessions: 'companies/sessions',
+                registrations: 'companies/registrations'
+              }
   devise_for :users,
              controllers: {
                sessions: 'users/sessions',
                registrations: 'users/registrations'
              }
   get '/member-data', to: 'members#show'
+  get '/company-data', to: 'companies#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
