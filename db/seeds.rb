380e518f5794fa52e1599b727ad2dc3df42747d5
Smit Akhani <smit.akhani@bacancy.com>
2022-06-19 20:07:47 +0530

    db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-user=User.create(email:"admin@admin.com",password:"123456",password_confirmation:"123456")
+user=User.create!(email:"admin@admin.com",password:"123456",password_confirmation:"123456")
 user.add_role :admin
