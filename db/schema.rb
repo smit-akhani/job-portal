528f5bace63c86ca02caa8d11811a1c5eb86f651
Yash Garala <yash.garala@bacancy.com>
2022-06-20 11:35:26 +0530

    Job alert crud is done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_18_120532) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_20_043546) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -105,6 +105,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_18_120532) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_experiences_on_user_id"
+  end
+
+  create_table "job_alerts", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "job_title"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_job_alerts_on_user_id"
   end
 
   create_table "job_managers", force: :cascade do |t|
@@ -223,6 +232,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_18_120532) do
   add_foreign_key "company_details", "companies"
   add_foreign_key "educations", "users"
   add_foreign_key "experiences", "users"
+  add_foreign_key "job_alerts", "users"
   add_foreign_key "job_managers", "companies"
   add_foreign_key "job_managers", "users"
   add_foreign_key "jobs", "companies"
---
+++ test/controllers/job_alerts_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class JobAlertsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
