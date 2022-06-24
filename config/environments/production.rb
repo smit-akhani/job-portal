6984acc5902a2133e060bb271144f72474a8fecb
Smit Akhani <smit.akhani@bacancy.com>
2022-06-24 12:15:40 +0530

    heroku redirect

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: "localhost:3000" }
+  config.action_mailer.default_url_options = { host: "https://job-portal-rails.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.default_options = {cc: 'yash.garala@bacancy.com'}
